eed96982c697f56d7854b623ec6adb1bf15c4d73
Jaeyeon Kim <amelia1023@naver.com>
2022-06-17 11:58:21 +0900

    api json load

--- map/urls.py
+++ map/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', index, name="index"),
     path('data/', data, name="data"),
+    path('api/', APItest, name="APItest"),
 ]
--- map/views.py
+++ map/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-import os
+import os, requests, json
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,14 @@ def index(request):
 
 def data(request):
     return render(request, 'data.json')
+
+def APItest(request):
+    requestData = requests.get('http://api.vworld.kr/req/data?key=5CC27E65-1081-3E65-A3F8-1EDD66DE7ECF&domain=http:127.0.0.1:8000&service=data&version=2.0&request=getfeature&format=json&size=10&page=1&data=LT_C_ADSIDO_INFO&attrfilter=ctprvn_cd:=:41&columns=ctprvn_cd,ctp_kor_nm,ctp_eng_nm,ag_geom&geometry=true&attribute=true')
+    jsonData = None
+    if requestData.status_code == 200:
+        jsonData = requestData.json()
+        f = open("./map/templates/data.json", 'w')
+        f.write(str(jsonData.get('response').get('result').get('featureCollection')))
+        f.close()
+        print(requestData.status_code, "Request OK")
+    return HttpResponse(requestData)
