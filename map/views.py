96db2b98de6b4388e646049f5cfbaff12776997d
Jaeyeon Kim <amelia1023@naver.com>
2022-06-17 10:33:22 +0900

    geojson load

--- map/views.py
+++ map/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import os
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def geojson(request):
+    return render(request, 'data.json')
